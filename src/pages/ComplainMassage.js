373f415e2d47c65763e4f4d5ec0dfbd7ba01d354
dani <danidewantara2@gmail.com>
2022-04-01 13:01:49 +0700

    update

--- src/pages/ComplainMassage.js
+++ src/pages/ComplainMassage.js
@@ -7,23 +7,23 @@ const ComplainMassage = () => {
     <div >
       <NavbarAdmin />
       <Row style={{ height: '80vh' }}>
-                <Col className='text-white flex ' style={{ borderRight: '1px solid white' }} sm={4}>
-                    <div className='layout-side'>
-                        <div className='complain-left' >
-                            <img src="assets/Ellipse1.png" alt="" />
-                            <h6>Admin</h6>
-                            <p>Yes, Is there anything I can help</p>
-                        </div>
-                    </div>
-                </Col>
-                <Col className='text-white flex ' sm={8}>
-                    <div>
-                      <div className="side-right-complain">
-                            <input style={{marginTop : '100px'}} type="text" placeholder='Send Message' />
-                      </div>
-                    </div>
-                </Col>
-            </Row>
+        <Col className='text-white flex ' style={{ borderRight: '1px solid white' }} sm={4}>
+          <div className='layout-side'>
+            <div className='complain-left' >
+              <img src="assets/Ellipse1.png" alt="" />
+              <h6>Admin</h6>
+              <p>Yes, Is there anything I can help</p>
+            </div>
+          </div>
+        </Col>
+        <Col className='text-white flex ' sm={8}>
+          <div>
+            <div className="side-right-complain">
+              <input style={{ marginTop: '100px' }} type="text" placeholder='Send Message' />
+            </div>
+          </div>
+        </Col>
+      </Row>
     </div>
   )
 }
